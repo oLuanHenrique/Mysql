4675509fd904c13faed957ad2f58ceec34172179
Luan Henrique <115194508+oLuanHenrique@users.noreply.github.com>
2023-06-18 22:36:31 -0300

    resolvendo os bugs e erros e testando os comandos para apresentação. Adicionei conteúdo ao arquivo README. Fiz a configuração dos eslintrc.json

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # Mysql
 Integração do express com o Mysql 
+
+# Aplicação Node.js CRUD
+
+Esta é uma aplicação Node.js CRUD que demonstra endpoints básicos de API para gerenciar clientes e produtos em um banco de dados MySQL.
+
+## Dependências
+
+Certifique-se de ter as seguintes dependências instaladas:
+
+- [cookie-parser](https://www.npmjs.com/package/cookie-parser)
+- [debug](https://www.npmjs.com/package/debug)
+- [dotenv](https://www.npmjs.com/package/dotenv)
+- [express](https://www.npmjs.com/package/express)
+- [http-errors](https://www.npmjs.com/package/http-errors)
+- [jade](https://www.npmjs.com/package/jade)
+- [morgan](https://www.npmjs.com/package/morgan)
+- [mysql2](https://www.npmjs.com/package/mysql2)
+
+## Dependências de Desenvolvimento
+
+As seguintes dependências de desenvolvimento são necessárias:
+
+- [eslint](https://www.npmjs.com/package/eslint)
+- [eslint-config-google](https://www.npmjs.com/package/eslint-config-google)
+- [nodemon](https://www.npmjs.com/package/nodemon)
+
+## Como Começar
+
+Instale as dependências:
+    - cd node-crud-app
+    - npm install
+
+No arquivo .env na raiz do projeto tem as configurações das variáveis de ambiente:
+    - DB_HOST=localhost
+    - DB_USER=root
+    - DB_PASSWORD=sua-senha
+    - DB_DATABASE=crud_app
+
+Execute a aplicação:
+    npm start
+
+Abra seu navegador e acesse http://localhost:3000 para ver a aplicação.
+
+Endpoints da API utilizados: 
+
+    GET /: Endpoint padrão que responde com uma mensagem de boas-vindas.
+    GET /clientes: Obter todos os clientes.
+    POST /clientes: Criar um novo cliente.
+    PUT /clientes/:id: Atualizar um cliente por ID.
+    DELETE /clientes/:id: Excluir um cliente por ID.
+    GET /produtos: Obter todos os produtos.
+    POST /produtos: Criar um novo produto.
+    PUT /produtos/:id: Atualizar um produto por ID.
+    DELETE /produtos/:id: Excluir um produto por ID.
+
+Estrutura do Projeto:
+
+    O projeto é estruturado da seguinte forma:
+
+    configs/: Arquivos de configuração.
+    controllers/: Arquivos que lidam com a lógica de negócios de cada endpoint.
+    middlewares/: Middlewares de validação de requisição.
+    models/: Arquivos SQL para criação do banco de dados, tabelas e inserção de dados.
+    routes/: APIs e endpoints.
+    services/: Acesso e manipulação do banco de dados.
+    views/: Camada de apresentação.
+
+Arquivos Incluídos:
+    Os seguintes arquivos obrigatórios devem estar presentes na raiz do projeto:
+    .env: Arquivo de configuração das variáveis de ambiente.
+    .eslintrc.json: Arquivo de configura
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Configuração do banco de dados
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'sua_senha',
-  database: 'sua_database',
+    host: 'localhost',
+    user: 'root',
+    password: 'sua_senha',
+    database: 'sua_database',
 });
 
 // Criação da aplicação Express
@@ -15,64 +16,71 @@ app.use(express.json());
 
 // Endpoint no caminho padrão "/"
 app.get('/', (req, res) => {
-  res.send('Bem-vindo à minha aplicação Node.js!');
+    res.send('Bem-vindo à minha aplicação Node.js!');
 });
 
 // Endpoints "/clientes" e "/produtos"
 app.route('/clientes')
-  .get((req, res) => {
+    .get((req, res) => {
     // Consulta os clientes no banco de dados
-    connection.query('SELECT * FROM clientes', (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Erro ao buscar clientes no banco de dados' });
-      } else {
-        res.json(results);
-      }
-    });
-  })
-  .post((req, res) => {
+        connection.query('SELECT * FROM clientes', (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error:
+            'Erro ao buscar clientes no banco de dados' });
+            } else {
+                res.json(results);
+            }
+        });
+    })
+    .post((req, res) => {
     // Insere um novo cliente no banco de dados
-    const { nome, email } = req.body;
-    connection.query('INSERT INTO clientes (nome, email) VALUES (?, ?)', [nome, email], (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Erro ao inserir cliente no banco de dados' });
-      } else {
-        res.sendStatus(201);
-      }
-    });
-  })
-  .put((req, res) => {
+        const { nome, email } = req.body;
+        connection.query('INSERT INTO clientes (nome, email) VALUES (?, ?)',
+            [nome, email], (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error:
+                        'Erro ao inserir cliente no banco de dados' });
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+    })
+    .put((req, res) => {
     // Atualiza um cliente no banco de dados
-    const { id, nome, email } = req.body;
-    connection.query('UPDATE clientes SET nome = ?, email = ? WHERE id = ?', [nome, email, id], (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Erro ao atualizar cliente no banco de dados' });
-      } else {
-        res.sendStatus(200);
-      }
-    });
-  })
-  .delete((req, res) => {
+        const { id, nome, email } = req.body;
+        connection.query('UPDATE clientes SET nome = ?, email = ? WHERE id = ?',
+            [nome, email, id], (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error:
+                    'Erro ao atualizar cliente no banco de dados' });
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+    })
+    .delete((req, res) => {
     // Deleta um cliente do banco de dados
-    const { id } = req.body;
-    connection.query('DELETE FROM clientes WHERE id = ?', [id], (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Erro ao deletar cliente no banco de dados' });
-      } else {
-        res.sendStatus(200);
-      }
+        const { id } = req.body;
+        connection.query('DELETE FROM clientes WHERE id = ?',
+            [id], (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error:
+                    'Erro ao deletar cliente no banco de dados' });
+                } else {
+                    res.sendStatus(200);
+                }
+            });
     });
-  });
 
-app.route('/produtos')
-  // Implemente de forma semelhante aos endpoints "/clientes"
+app.route('/produtos');
+// Implemente de forma semelhante aos endpoints "/clientes"
 
 // Inicialização do servidor
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
